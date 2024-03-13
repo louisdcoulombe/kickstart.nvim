f3e8bbd4355b627be47f64e0eea7a8fb493219a0
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-03-12 23:22:31 -0400

    feat(telescope): More telescope functions

--- init.lua
+++ init.lua
@@ -512,9 +512,19 @@ require('lazy').setup {
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
       vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>fF', function()
+        require('telescope.builtin').find_files { hidden = true, no_ignore = true }
+      end, { desc = 'Find all files' })
       -- vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fG', function()
+        require('telescope.builtin').live_grep {
+          additional_args = function(args)
+            return vim.list_extend(args, { '--hidden', '--no-ignore' })
+          end,
+        }
+      end, { desc = '[F]ind words in all files' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[F]ind git files' })
