a372d4d2d39242f99665a4ed4e9d97c4c253ecc5
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-03-12 23:27:30 -0400

    feat(neotree): Removed annoying buffer when opening folder

--- init.lua
+++ init.lua
@@ -428,6 +428,18 @@ require('lazy').setup {
 
       vim.keymap.set('n', '<leader>e', ':Neotree toggle<cr>', { desc = 'Toggle neotree' })
     end,
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == 'directory' then
+          require('neo-tree').setup {
+            filesystem = {
+              hijack_netrw_behavior = 'open_current',
+            },
+          }
+        end
+      end
+    end,
   },
 
   -- NOTE: Plugins can specify dependencies.
