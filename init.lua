e9a5e7c114aef605d27b299a5c2e95f72e96f139
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-03-12 22:58:21 -0400

    feat(gitsigns): Configured gitsigns

--- init.lua
+++ init.lua
@@ -243,11 +243,9 @@ require('lazy').setup {
   -- keys can be used to configure plugin behavior/loading/etc.
   --
   -- Use `opts = {}` to force a plugin to be loaded.
-  --
   --  This is equivalent to:
   --    require('Comment').setup({})
 
-  -- "gc" to comment visual regions/lines
   {
     'numToStr/Comment.nvim',
     config = function()
@@ -324,6 +322,51 @@ require('lazy').setup {
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      }
+
+      vim.keymap.set('n', ']g', function()
+        require('gitsigns').next_hunk()
+      end, { desc = 'Next Git hunk' })
+      vim.keymap.set('n', '[g', function()
+        require('gitsigns').prev_hunk()
+      end, { desc = 'Previous Git hunk' })
+      vim.keymap.set('n', '<leader>gl', function()
+        require('gitsigns').blame_line()
+      end, { desc = 'View Git blame' })
+      vim.keymap.set('n', '<leader>gL', function()
+        require('gitsigns').blame_line { full = true }
+      end, { desc = 'View full Git blame' })
+      vim.keymap.set('n', '<leader>gp', function()
+        require('gitsigns').preview_hunk()
+      end, { desc = 'Preview Git hunk' })
+      vim.keymap.set('n', '<leader>gh', function()
+        require('gitsigns').reset_hunk()
+      end, { desc = 'Reset Git hunk' })
+      vim.keymap.set('n', '<leader>gr', function()
+        require('gitsigns').reset_buffer()
+      end, { desc = 'Reset Git buffer' })
+      vim.keymap.set('n', '<leader>gs', function()
+        require('gitsigns').stage_hunk()
+      end, { desc = 'Stage Git hunk' })
+      vim.keymap.set('n', '<leader>gS', function()
+        require('gitsigns').stage_buffer()
+      end, { desc = 'Stage Git buffer' })
+      vim.keymap.set('n', '<leader>gu', function()
+        require('gitsigns').undo_stage_hunk()
+      end, { desc = 'Unstage Git hunk' })
+      vim.keymap.set('n', '<leader>gd', function()
+        require('gitsigns').diffthis()
+      end, { desc = 'View Git diff' })
+    end,
     opts = {
       signs = {
         add = { text = '+' },
