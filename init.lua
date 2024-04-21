3487dff68641a9107b3f46f807238f1798cf19e6
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-04-21 15:40:46 -0400

    feat(lsp-signature): Added lsp-signature plugin

--- init.lua
+++ init.lua
@@ -893,6 +893,14 @@ require('lazy').setup {
       }
     end,
   },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
   { 'folke/neodev.nvim', opts = {} },
   {
     'rcarriga/nvim-dap-ui',
