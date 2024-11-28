1f0e248241bedc5c67c86524cf0d78880f54ed4b
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2024-11-27 19:21:28 -0500

    fix(refactor): Removed duplicate which-key group for R

--- init.lua
+++ init.lua
@@ -443,12 +443,11 @@ require('lazy').setup {
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ebugger', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]efactor', _ = 'which_key_ignore' },
         ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
         ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>x'] = { name = '[X]custom', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = 'Refactor', _ = 'which_key_ignore' },
       }
     end,
   },
